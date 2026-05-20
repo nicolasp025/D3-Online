7ce7d2c218ff4dbb0eba3fdca7d90de507a44acf
Nicolas Potel <nicolas.potel@inria.fr>
2026-05-20 13:46:51 +0200

    fix update in frame tables when changing divergence

--- src/components/divergence-visualizer/DivergenceItem.tsx
+++ src/components/divergence-visualizer/DivergenceItem.tsx
@@ -1,6 +1,5 @@
 import { useDivergence } from "../../contexts/DivergenceContext";
-import { useStacks } from "../../contexts/StacksContext";
-import type { D3Divergence, D3FlowDivergence, D3StateDivergence } from "../../models/divergence";
+import type { D3Divergence, D3StateDivergence } from "../../models/divergence";
 import expand_arrow from "../../assets/icons/arrow_drop_down.svg";
 import { forwardRef, useState } from "react";
 
@@ -10,32 +9,16 @@ interface DivergenceItemProps {
 
 const DivergenceItem = forwardRef<HTMLDivElement, DivergenceItemProps>(({ divergence }, ref) => {
   const { selectedDivergence, setSelectedDivergence, isFlowDivergence } = useDivergence();
-  const { setOriginalPosition, setModifiedPosition } = useStacks();
 
   const isExpandable = !isFlowDivergence(divergence);
   const [isExpanded, setExpanded] = useState(false);
-
-  /**
-   * Select the specified divergence and sets the original and modified positions according to the divergence.
-   * @param d The specified divergence to select.
-   */
-  const selectDivergenceInTables = (d: D3Divergence) => {
-    setSelectedDivergence(d);
-    if (isFlowDivergence(d)) {
-      setOriginalPosition((d as D3FlowDivergence).originalPosition.start);
-      setModifiedPosition((d as D3FlowDivergence).modifiedPosition.start);
-    } else {
-      setOriginalPosition((d as D3StateDivergence).originalPosition);
-      setModifiedPosition((d as D3StateDivergence).modifiedPosition);
-    }
-  };
 
   return (
     <div
       ref={ref}
       className={`divergence-wrapper`}
       onClick={() => {
-        selectDivergenceInTables(divergence);
+        setSelectedDivergence(divergence);
       }}
     >
       <div
