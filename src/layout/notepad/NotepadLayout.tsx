cac9a931bfcc5876ce565806ac5bc6a8e0041763
Nicolas Potel <nicolas.potel@inria.fr>
2026-05-26 15:31:47 +0200

    notepad page ok

--- src/layout/notepad/NotepadLayout.tsx
+++ src/layout/notepad/NotepadLayout.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import "./NotepadLayout.css";
+
 const NotepadLayout = () => {
-  return <>Notepad here</>;
+  const [content, setContent] = useState<string>("");
+  return (
+    <div className="container">
+      <textarea
+        id="notepad"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="Write your thoughts here ..."
+      />
+    </div>
+  );
 };
 
 export default NotepadLayout;
