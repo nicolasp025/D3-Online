f5ebf19576102a1c3ec066003f16ac31c7b995e0
Nicolas Potel <nicolas@potel.dev>
2026-06-10 14:52:06 +0200

    color legend in tree

--- src/layout/tree/TreeLayout.tsx
+++ src/layout/tree/TreeLayout.tsx
@@ -1,7 +1,48 @@
 import "./TreeLayout.css";
 import DivergenceTree from "../../components/divergence-tree/DivergenceTree";
+import { TREE_CONFIG } from "../../components/divergence-tree/tree-config";
 
 const TreeLayout = () => {
-    return <DivergenceTree />;
+  return (
+    <div className="tree-layout">
+      <DivergenceTree />
+      <dl className="legend">
+        <div className="legend-item">
+          <dt className="legend-color state-div">
+            <svg>
+              <circle
+                className="state-div"
+                r={TREE_CONFIG.DOT_RADIUS}
+                cx={10}
+                cy={10}
+              />
+            </svg>
+          </dt>
+          <dd className="legend-label">State divergence</dd>
+        </div>
+        <div className="legend-item">
+          <dt className="legend-color">
+            <svg>
+              <circle
+                className="flow-div"
+                r={TREE_CONFIG.DOT_RADIUS}
+                cx={10}
+                cy={10}
+              />
+            </svg>
+          </dt>
+          <dd className="legend-label">Flow divergence</dd>
+        </div>
+        <div className="legend-item">
+          <dt>
+            <svg className="legend-color">
+              <line x1={0} y1={12} x2={20} y2={12} />
+            </svg>
+          </dt>
+          <dd className="legend-label">Divergence / convergence</dd>
+        </div>
+      </dl>
+    </div>
+  );
 };
 export default TreeLayout;
