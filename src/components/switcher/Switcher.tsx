5b34247c10218cf9ccbb57a06d05934e2296213b
Nicolas Potel <nicolas@potel.dev>
2026-06-29 14:21:11 +0200

    switcher component & some ui fixes on query page

---
+++ src/components/switcher/Switcher.tsx
@@ -0,0 +1,63 @@
+import { useState, useRef, useEffect } from "react";
+import "./Switcher.css";
+
+interface SwitcherProps<T> {
+    items: T[];
+    onChange: (item: T) => void;
+    renderLabel: (item: T) => string;
+}
+
+const Switcher = <T,>({ items, onChange, renderLabel }: SwitcherProps<T>) => {
+    if (items.length == 0) return <></>;
+
+    const [indicatorStyle, setIndicatorStyle] = useState({ left: 0, width: 0 });
+    const containerRef = useRef<HTMLDivElement>(null);
+    const selectedItemRef = useRef<HTMLDivElement>(null);
+
+    const [selectedItem, setSelectedItem] = useState<T>(items[0]);
+
+    useEffect(() => {
+        const containerElement = containerRef.current;
+        const selectedElement = selectedItemRef.current;
+
+        if (!selectedElement || !containerElement) return;
+
+        const containerRect = containerElement.getBoundingClientRect();
+        const selectedRect = selectedElement.getBoundingClientRect();
+
+        setIndicatorStyle({
+            left: selectedRect.left - containerRect.left,
+            width: selectedRect.width,
+        });
+    }, [selectedItem]);
+
+    return (
+        <div className="switcher-container" ref={containerRef}>
+            <div
+                className="switcher-indicator"
+                style={{
+                    left: indicatorStyle.left,
+                    width: indicatorStyle.width,
+                }}
+            />
+
+            <div className="switcher-items">
+                {items.map((item, index) => (
+                    <div
+                        className="switcher-item"
+                        key={index}
+                        ref={item == selectedItem ? selectedItemRef : null}
+                        onClick={() => {
+                            setSelectedItem(item);
+                            onChange(item);
+                        }}
+                    >
+                        {renderLabel(item)}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Switcher;
--- src/layout/query/QueryLayout.tsx
+++ src/layout/query/QueryLayout.tsx
@@ -3,27 +3,46 @@ import { useInspector } from "../../hooks/useInspector";
 import "../../models/queries/index";
 import "./QueryLayout.css";
 import { useQuery } from "../../hooks/useQuery";
+import Switcher from "../../components/switcher/Switcher";
+import { useEffect, useState } from "react";
 
 const QueryLayout = () => {
   const { openInspectorOn } = useInspector();
 
   const { queries, addQuery } = useQuery();
 
+  const [selectedItem, setSelectedItem] = useState<string>("testtest3");
+
   return (
-      <div className="query-layout">
-        <div className="query-container container">
-          <TabDisplayer
-            tabs={queries.map((query, index) => {
-              return {
-                id: index,
-                title: query.name,
-                content: <>{query.description}</>,
-              };
-            })}
-            onAddTab={addQuery}
-          ></TabDisplayer>
-        </div>
+    <div className="query-layout">
+      <div className="query-container container">
+        <TabDisplayer
+          tabs={queries.map((query, index) => {
+            return {
+              id: index,
+              title: query.name,
+              content: (
+                <>
+                  <div className="query-rows">
+                    <div className="query-row">
+                      <span>What are you investigating ?</span>
+                      <Switcher
+                        items={["Source code", "Execution behavior"]}
+                        onChange={(item) => {
+                          setSelectedItem(item);
+                        }}
+                        renderLabel={(item) => item}
+                      />
+                    </div>
+                  </div>
+                </>
+              ),
+            };
+          })}
+          onAddTab={addQuery}
+        ></TabDisplayer>
       </div>
+    </div>
   );
 };
 
